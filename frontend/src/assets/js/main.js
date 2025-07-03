4829746a689c592d7013288f6b4afedc587b535e
Ashmitha <114351118+ash-dsouza@users.noreply.github.com>
2025-07-04 00:08:16 +0530

    Tweaking main.js

--- frontend/src/assets/js/main.js
+++ frontend/src/assets/js/main.js
@@ -80,13 +80,29 @@
       window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
     }
   }
-  scrollTop.addEventListener('click', (e) => {
-    e.preventDefault();
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
+  // scrollTop.addEventListener('click', (e) => {
+  //   e.preventDefault();
+  //   window.scrollTo({
+  //     top: 0,
+  //     behavior: 'smooth'
+  //   });
+  // });
+
+  // Changed to load homepage
+(function() {
+  const scrollTop = document.querySelector('.scroll-top');
+
+  if (scrollTop) {
+    scrollTop.addEventListener('click', (e) => {
+      e.preventDefault();
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
     });
-  });
+  }
+})();
+
 
   window.addEventListener('load', toggleScrollTop);
   document.addEventListener('scroll', toggleScrollTop);
