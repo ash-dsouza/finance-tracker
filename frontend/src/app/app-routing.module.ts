0480e31362c9151c83226f24a8012309c3de916c
Ashmitha <114351118+ash-dsouza@users.noreply.github.com>
2025-07-06 18:38:14 +0530

    setting up routing for login page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
-  
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
 ];
 
 @NgModule({
