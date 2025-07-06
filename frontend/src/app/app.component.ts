c20f4f4ce425484ce243eec8817802b7097ea0fc
Ashmitha <114351118+ash-dsouza@users.noreply.github.com>
2025-07-06 18:42:29 +0530

    update layout to conditionally hide header and footer on login page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from './pages/login/login.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'SpendWise';
+  public showLayout = true;
+
+  onRouteChange(component: any) {
+  this.showLayout = !(component instanceof LoginComponent);
+}
 }
