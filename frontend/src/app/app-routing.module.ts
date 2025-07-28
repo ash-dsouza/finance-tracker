8b2f804eeaba741089946fc4cf6d4374031cc5e0
Ashmitha <114351118+ash-dsouza@users.noreply.github.com>
2025-07-28 23:42:53 +0530

    Updating routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AddEntryComponent } from './pages/add-entry/add-entry.component';
 
 const routes: Routes = [
+   {path: '', component: HomeComponent},
+   {path: 'login', component: LoginComponent},
+   {path: 'dashboard', component: DashboardComponent},
+   {path: 'add-entry', component: AddEntryComponent},
   
 ];
 
