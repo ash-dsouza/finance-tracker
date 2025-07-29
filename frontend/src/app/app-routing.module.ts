5cb54c782e3e042d410e4c90e0f9bc61906b9b5f
Ashmitha <114351118+ash-dsouza@users.noreply.github.com>
2025-07-29 18:57:01 +0530

    Link login and signup pages

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { AddEntryComponent } from './pages/add-entry/add-entry.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 const routes: Routes = [
    {path: '', component: HomeComponent},
    {path: 'login', component: LoginComponent},
    {path: 'dashboard', component: DashboardComponent},
    {path: 'add-entry', component: AddEntryComponent},
-  
+   { path: 'signup', component: SignUpComponent }
 ];
 
 @NgModule({
--- frontend/src/app/pages/sign-up/sign-up.component.ts
+++ frontend/src/app/pages/sign-up/sign-up.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent implements OnInit {
+export class SignUpComponent {
+  name = '';
+    email = '';
+    password = '';
+    confirmPassword = '';
 
-  constructor() { }
+    constructor(private router: Router) {}
 
-  ngOnInit(): void {
-  }
+    onSignup() {
+      if (this.password !== this.confirmPassword) {
+        alert('Passwords do not match!');
+        return;
+      }
+
+      // You can store in localStorage or send to API here
+      console.log('Signup Successful:', this.name, this.email);
+      alert('Account created!');
+      this.router.navigate(['/login']);
+    }
 
 }
